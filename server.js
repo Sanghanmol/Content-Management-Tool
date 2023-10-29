89dad0b6d376216067e63058c92cf910dee892b4
Anmol Sangha <anmolsangha02@gmail.com>
2023-10-29 18:23:11 +0530

    first commit

--- server.js
+++ server.js
@@ -16,11 +16,18 @@ connectDB();
 
 //rest objecct
 const app = express();
+const path = require("path");
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+//static files
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //routes
 app.use("/api/user", userRoutes);
