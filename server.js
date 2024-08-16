9f222148579d68597561535fef2f4c5859d19411
Anmol Sangha <anmolsangha02@gmail.com>
2024-08-16 17:02:37 +0530

    make code deploy ready

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require('path');
 
 //env config
 dotenv.config();
@@ -25,6 +26,21 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/user", userRoutes);
 app.use("/api/blog", blogRoutes);
+
+//deploy
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // Port
 const PORT = process.env.PORT || 8080;
